6b73ae1f97b03104b0f31afe599efd9e8365dc14
Kannan Kalidasan <kannanpoem1984@gmail.com>
2024-08-24 08:39:02 +0100

    image tag maintained

--- app/SponserCoffee.js
+++ app/SponserCoffee.js
@@ -1,18 +1,11 @@
-import Image from 'next/image'
 
 const SponserCoffee = ({}) => {
   return (
     <div>
       <a href="https://www.buymeacoffee.com/kannandreams" target="_blank">
-        {/* <img
+        <img
           src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
           alt="Buy Me A Coffee"
-          style={{ height: '40px', width: '160px !important' }}
-        /> */}
-        <Image
-          src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
-          alt="Buy Me A Coffee"
-          layout="responsive" // or use "intrinsic" or "fixed" depending on your needs
           style={{ height: '40px', width: '160px !important' }}
         />
       </a>

8fb59a11f171ea072cf7d863b53822c6f7568c6a
kannandreams <kannanpoem1984@gmail.com>
2025-10-20 00:18:36 +0100

    fixing gh page

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    basePath: '/life-weeks-visualization',
     reactStrictMode: true,
     images: {
+      unoptimized: true,
       remotePatterns: [
         {
           protocol: "https",
